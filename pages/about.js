df6424d86951a8bd0ba210d5b83b05f552b889a1
RASHAD ABDULBARR <rashadabdulbarr@RASHADs-MacBook-Pro.local>
2021-05-24 09:14:18 -0400

    first

---
+++ pages/about.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const about = () => {
+	return (
+		<div>
+			<h1>About</h1>
+			<p>
+				Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris at dictum arcu. Cras ac malesuada nisi.
+				Maecenas fringilla fermentum turpis, vitae molestie felis iaculis eget. Vestibulum semper dictum ante in
+				volutpat. Suspendisse non neque suscipit, efficitur ligula eget, ultrices elit. Mauris faucibus, est
+				egestas imperdiet dignissim, dolor mi tempor nibh, vitae dignissim enim mauris quis justo. Pellentesque
+				eleifend odio nisl, vel fermentum orci volutpat sed. Integer congue mauris et erat consectetur mollis.
+				Pellentesque dictum molestie fringilla.
+			</p>
+		</div>
+	);
+};
+
+export default about;
